b4c8f1362f960f2ca54396d549cf928ed8481535
Zuzanna <zuzanna.pawlowska@riotters.com>
2024-03-27 14:16:00 +0100

    refactor: data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState,useEffect } from 'react'
+
+import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchData = async () => {
+  const response = await fetch(
+    "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+  );
+  const tags = await response.json();
+
+  return tags;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [posts, setPosts] = useState(null)
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["tags"],
+    queryFn: fetchData,
+  });
+
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul>
+      {data.items.map((el, i) => (
+        <li key={i++}>{el.name}</li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
